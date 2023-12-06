5dd0306350e677badb5c2d335e1d71548a688d55
Brijrajsinh01 <brijrajraolji@gmail.com>
2023-12-06 23:35:09 +0530

    Changes

---
+++ ur3/launch/gazebo.launch.py
@@ -0,0 +1,19 @@
+import launch,os
+from launch import LaunchDescription
+from launch_ros.actions import Node, ComposableNodeContainer
+from launch_ros.descriptions import ComposableNode
+from launch.launch_description_sources import PythonLaunchDescriptionSource
+from launch.actions import IncludeLaunchDescription
+from ament_index_python.packages import get_package_share_directory
+def generate_launch_description():
+    # Include the Gazebo launch file, provided by the gazebo_ros package
+    gazebo = IncludeLaunchDescription(
+                PythonLaunchDescriptionSource([os.path.join(
+                    get_package_share_directory('gazebo_ros'), 'launch', 'gazebo.launch.py')]),
+                    launch_arguments={'world': './src/my_bot/worlds/obstacle.world'}.items()  # Specify the world file name here.
+             )
+    return LaunchDescription([
+        gazebo,
+        
+        
+    ])
